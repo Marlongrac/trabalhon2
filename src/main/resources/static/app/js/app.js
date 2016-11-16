f6412a979eeb6ee4e63c295d0d16797233a7b7a0
1001252191 <1001252191@PR014LAB002-008.PEDAGOGICO.NET>
2016-11-16 21:25:36 -0200

    Criação de tela de login e refactory

--- src/main/resources/static/app/js/app.js
+++ src/main/resources/static/app/js/app.js
@@ -1,10 +1,27 @@
 var app = angular.module('app', ['ngResource', 'ngRoute', 'fxpicklist']);
 
+//Interceptor de autenticação
+app.factory('authInterceptorService', ['$q', function ($q){
+    return {
+        responseError: function (rejection) {
+            if (rejection.status === 401) { //Acesso negado
+            	//Redireciona para a tela de login
+                location.href = "/";
+            }
+            return $q.reject(rejection);
+        }
+    };
+}]);
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+}]);
+
 /**
- * Configuração das Rotas (páginas do sistema)
+ * Configuração das Rotas Principais
  */
 app.config(['$routeProvider', function($routerProvider){
 	$routerProvider
+		
 		.when('/', {
 			templateUrl: 'home.html'
 		})
